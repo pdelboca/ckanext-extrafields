88f05f8ea10c7123a783f0d98accd9e492121a63
pdelboca <patriciodelboca@gmail.com>
2019-04-05 12:08:12 -0300

    Adding extra fields.

--- ckanext/extrafields/plugin.py
+++ ckanext/extrafields/plugin.py
@@ -1,13 +1,114 @@
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+# encoding: utf-8
+
+import ckan.plugins as p
+import ckan.plugins.toolkit as tk
 
 
-class ExtrafieldsPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+def create_country_codes():
+    '''Create country_codes vocab and tags, if they don't exist already.
+    Note that you could also create the vocab and tags using CKAN's API,
+    and once they are created you can edit them (e.g. to add and remove
+    possible dataset country code values) using the API.
+    '''
+    user = tk.get_action('get_site_user')({'ignore_auth': True}, {})
+    context = {'user': user['name']}
+    try:
+        data = {'id': 'country_codes'}
+        tk.get_action('vocabulary_show')(context, data)
+    except tk.ObjectNotFound:
+        data = {'name': 'country_codes'}
+        vocab = tk.get_action('vocabulary_create')(context, data)
+        for tag in (u'uk', u'ie', u'de', u'fr', u'es'):
+            data = {'name': tag, 'vocabulary_id': vocab['id']}
+            tk.get_action('tag_create')(context, data)
 
-    # IConfigurer
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'extrafields')
+def country_codes():
+    '''Return the list of country codes from the country codes vocabulary.'''
+    create_country_codes()
+    try:
+        country_codes = tk.get_action('tag_list')(
+                data_dict={'vocabulary_id': 'country_codes'})
+        return country_codes
+    except tk.ObjectNotFound:
+        return None
+
+
+class ExtrafieldsPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
+    p.implements(p.IDatasetForm)
+    p.implements(p.IConfigurer)
+    p.implements(p.ITemplateHelpers)
+
+    def _modify_package_schema(self, schema):
+        # Add our custom country_code metadata field to the schema.
+        schema.update({
+                'country_code': [
+                    tk.get_validator('ignore_missing'),
+                    tk.get_converter('convert_to_tags')('country_codes')]
+                })
+        # Add our custom_test metadata field to the schema, this one will use
+        # convert_to_extras instead of convert_to_tags.
+        schema.update({
+                'custom_text': [
+                    tk.get_validator('ignore_missing'),
+                    tk.get_converter('convert_to_extras')]
+                })
+        # Add our custom_resource_text metadata field to the schema
+        schema['resources'].update({
+                'custom_resource_text': [tk.get_validator('ignore_missing')]
+                })
+        return schema
+
+    def create_package_schema(self):
+        schema = super(ExtrafieldsPlugin, self).create_package_schema()
+        schema = self._modify_package_schema(schema)
+        return schema
+
+    def update_package_schema(self):
+        schema = super(ExtrafieldsPlugin, self).update_package_schema()
+        schema = self._modify_package_schema(schema)
+        return schema
+
+    def show_package_schema(self):
+        schema = super(ExtrafieldsPlugin, self).show_package_schema()
+
+        # Don't show vocab tags mixed in with normal 'free' tags
+        # (e.g. on dataset pages, or on the search page)
+        schema['tags']['__extras'].append(tk.get_converter('free_tags_only'))
+
+        # Add our custom country_code metadata field to the schema.
+        schema.update({
+            'country_code': [
+                tk.get_converter('convert_from_tags')('country_codes'),
+                tk.get_validator('ignore_missing')]
+            })
+
+        # Add our custom_text field to the dataset schema.
+        schema.update({
+            'custom_text': [
+                tk.get_converter('convert_from_extras'),
+                tk.get_validator('ignore_missing')]
+            })
+
+        schema['resources'].update({
+                'custom_resource_text': [tk.get_validator('ignore_missing')]
+            })
+        return schema
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
+
+    def package_types(self):
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
+
+    def update_config(self, config):
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        tk.add_template_directory(config, 'templates')
+
+    def get_helpers(self):
+        return {'country_codes': country_codes}
